f0849ce5e0fc1a88b25e91316d1ed109ab980f32
Ian Andre Aragon Saenz <43384876+IanAragonSaenz@users.noreply.github.com>
2022-05-18 22:29:52 -0500

    base code with SRP
    si

---
+++ src/movies/movie_storer_csv.py
@@ -0,0 +1,9 @@
+import csv
+
+def store_movies_csv(movies):
+    fields = ["preference_key", "movie_title", "star_cast", "rating", "year", "place", "vote", "link"]
+    with open("movie_results.csv", "w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=fields)
+        writer.writeheader()
+        for movie in movies:
+            writer.writerow({**movie})
---
+++ src/movies/movie_storer_postgre.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import datetime
+
+from movies.models import Movie, get_postgres_uri
+
+DEFAULT_SESSION_FACTORY = sessionmaker(
+    bind=create_engine(
+        get_postgres_uri(),
+        isolation_level="REPEATABLE READ",
+    )
+)
+session = DEFAULT_SESSION_FACTORY()
+db = scoped_session(session)
+
+def store_movies_postgre(movies):
+    i = 0
+    for movie in movies:
+        m = Movie(movie_id = i, preference_key = movie["preference_key"], movie_title = movie["movie_title"], rating = movie["rating"], year = movie["year"], create_time = datetime.datetime.now())
+        session.add(m)
+        i += 1
+
+    session.commit()    
+
+    
+    
