8cdd84bb93f9ab2d51df4bf7739cbdbc755a0649
Marcos Quintero <a00824603@itesm.mx>
2022-05-26 18:09:45 -0500

    template added y route app

--- src/movies/entrypoints/flask_app.py
+++ src/movies/entrypoints/flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from movies import models
+from flask import render_template
 
 app = Flask(__name__)
 models.start_mappers()
@@ -7,4 +8,26 @@ models.start_mappers()
 
 @app.route("/hello", methods=["GET"])
 def hello_world():
-    return "Hello World!", 200
+    return "hello", 200
+
+@app.route("/", methods=["GET"])
+def home(name=None):
+    
+    return render_template('home.html', name=name)
+
+
+@app.route("/movies", methods=["POST"])
+def movies(name=None):
+    category_value = {'comedy' : 1, 'drama' : 2, 'sci-fi' : 3, 'romantic' : 4, 'adventure' : 5}
+
+    category1 = category_value[request.form['category1']]
+    category2 = category_value[request.form['category2']]
+    category3 = category_value[request.form['category3']]
+
+    sorted = request.form['rated']
+
+    preference_key = category1 * category2 * category3 % 5 + 1
+
+    
+
+    return 'troll' 
